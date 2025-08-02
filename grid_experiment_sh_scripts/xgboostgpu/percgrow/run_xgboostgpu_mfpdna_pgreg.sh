d84b5c140ff088bf81b58483310216bd1858291a
Alex Wong <alexandra_wong@brown.edu>
2025-08-01 22:05:58 -0400

    Ready percgrowth for XGBGPU

--- grid_experiment_sh_scripts/xgboostgpu/percgrow/run_xgboostgpu_mfpdna_pgreg.sh
+++ grid_experiment_sh_scripts/xgboostgpu/percgrow/run_xgboostgpu_mfpdna_pgreg.sh
@@ -2,31 +2,31 @@
 
 #SBATCH --mail-type=ALL                                     #email notification
 #SBATCH --mail-user=alexandra_wong@brown.edu                #email address
-#SBATCH -J 20241017_mfpdna_xgboostgpu_pgreg_experiment      #job name
+#SBATCH -J 20250712_pg_mfpdna_xgboostgpu_pgreg_experiment      #job name
 
 #SBATCH --partition=gpu                                  #partition
 #SBATCH --gres=gpu:1                                      #request GPU
 #SBATCH -N 1                                                #number of nodes
 #SBATCH -c 1                                                #number of cores
 
-#SBATCH -t 2:00:00                                         #time limit (HH:MM:SS)
-#SBATCH --mem=175G                                          #memory per node
+#SBATCH -t 10:00:00                                         #time limit (HH:MM:SS)
+#SBATCH --mem=350G                                          #memory per node
 
 #SBATCH --array=1                                         #job array
 
 # Use '%A' for job array ID, '%J' for job ID, '%a' for array task ID
-#SBATCH --output=scratch/anticancer-synergy-prediction-scratch/experiments/20241017_xgboostgpu/mfpdna_xgboostgpu_pgreg_%a_slurm.out           #output file
-#SBATCH --error=scratch/anticancer-synergy-prediction-scratch/experiments/20241017_xgboostgpu/mfpdna_xgboostgpu_pgreg_%a_slurm.err            #error file
+#SBATCH --output=scratch/anticancer-synergy-prediction-scratch/experiments/20250712_pg_xgboostgpu/mfpdna_xgboostgpu_pgreg_%a_slurm.out           #output file
+#SBATCH --error=scratch/anticancer-synergy-prediction-scratch/experiments/20250712_pg_xgboostgpu/mfpdna_xgboostgpu_pgreg_%a_slurm.err            #error file
 
 source env/bin/activate
 
 SCRATCH="scratch/anticancer-synergy-prediction-scratch/"
-
-PYOF=$SCRATCH"experiments/20241017_xgboostgpu/mfpdna_xgboostgpu_pgreg"$SLURM_ARRAY_TASK_ID".txt"
-PLOT=$SCRATCH"experiments/20241017_xgboostgpu/mfpdna_xgboostgpu_pgreg"
+export PYTHONPATH="${PYTHONPATH}:~/cancer-drug-synergy-prediction/"
+PYOF=$SCRATCH"experiments/20250712_pg_xgboostgpu/mfpdna_xgboostgpu_pgreg"$SLURM_ARRAY_TASK_ID".txt"
+PLOT=$SCRATCH"experiments/20250712_pg_xgboostgpu/mfpdna_xgboostgpu_pgreg"
 
 # Run hyperparameter experiments using job array index to select hyperparameters in the vnn_experiments_oscar.py script
 # REMEMBER TO INDEX BY 1
 echo "Starting job $SLURM_ARRAY_TASK_ID on $HOSTNAME starting on `date`"
-python3 run_xgboostgpu_models.py --use_mfp --use_dna --use_pgreg --output_fp $PLOT"metrics.csv" > $PYOF
+python3 models/run/run_xgboostgpu_models.py --score 'PERCENTGROWTH' --use_mfp --use_dna --use_pgreg --output_fp $PLOT"metrics.csv" > $PYOF
 echo "Job $SLURM_ARRAY_TASK_ID ended at `date`"
