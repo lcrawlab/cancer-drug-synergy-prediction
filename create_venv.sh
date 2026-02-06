3ebd6d431e0c8422b700981574d06ac16eb7022d
Alex Wong <alexandra_wong@brown.edu>
2026-02-05 21:58:44 -0500

    New modules for Oscar migration

--- create_venv.sh
+++ create_venv.sh
@@ -3,8 +3,8 @@
 # install, create, and activate virtualenv called "env"
 python3 -m pip install virtualenv
 python3 -m venv env
-module load cuda/12.1.1 # - needed for running on gpus
-module load cudnn/8.7.0.84-11.8 # - needed for running on gpus
+module load cuda/12.6.0 # - needed for running on gpus
+module load cudnn/9.8.0.87-12 # - needed for running on gpus
 source env/bin/activate
 
 # update pip and install required packages
